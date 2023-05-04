2355e0622105a0fa1394690824aa6c3971a05f05
Andrew Efurhievwe <andyriles22@gmail.com>
2023-05-04 14:04:04 +0100

    minor stuff

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('prime nestjs')
-    .setDescription('Boilerplate for nestjs')
+    .setDescription('Ecommerce app work sample')
     .setVersion('1.0')
     .addTag('api')
     .build();
